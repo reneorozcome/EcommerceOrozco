9368e7907e7e24f41c3fb372d5620b9ce7ab6635
Rene Orozco <natosrene@gmail.com>
2022-02-25 02:48:35 -0500

    Desafío 2 Clase 3

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import NavBar from './NavBar'
+
+const App = () => {
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <section>
+
+            </section>
+            <footer>
+                © Todos los derechos reservados eCommerce Orozco<br />
+                con amor por reneorozco
+            </footer>
+        </>
+    )
+}
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <>
+            <h1><span className="material-icons">local_mall</span> eCommerce Orozco</h1>
+            <nav>
+                <a href="#"><span className="material-icons">home</span></a>
+                <a href="#">Afiches</a>
+                <a href="#">Figuras de acción</a>
+                <a href="#">Fondos de pantalla</a>
+                <a href="#">Llaveros</a>
+            </nav>
+        </>
+    )
+}
+export default NavBar
